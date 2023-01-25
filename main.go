6c6377328b664cec79e3d7c08eb4e94da15bcc84
masaki nakamura <gmk4n0n@gmail.com>
2023-01-25 22:46:01 +0900

    adding router (gorilla mux)

--- main.go
+++ main.go
@@ -4,14 +4,24 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello world from go server")
-	})
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/contacts", Contacts)
 
-	server := http.ListenAndServe(":8080", nil)
+	server := http.ListenAndServe(":8080", router)
 
 	log.Fatal(server)
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello world from go server")
+}
+
+func Contacts(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "contacts page")
 }
