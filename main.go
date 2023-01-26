f9cb6a13d437c2eb0981a6b6568198cf08e56877
masaki nakamura <gmk4n0n@gmail.com>
2023-01-26 19:05:36 +0900

    moving functions to another class

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 
@@ -19,29 +17,4 @@ func main() {
 	server := http.ListenAndServe(":8080", router)
 
 	log.Fatal(server)
-}
-
-func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello world from go server")
-}
-
-func Contacts(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "contacts page")
-}
-
-func MovieList(w http.ResponseWriter, r *http.Request) {
-	movies := Movies{
-		Movie{"inception", 2010, "Nolan"},
-		Movie{"fast and furious", 2015, "Diesel"},
-		Movie{"Ninja turtles", 2014, "Liebesman"},
-	}
-
-	//fmt.Fprintf(w, "list of movies")
-	json.NewEncoder(w).Encode(movies)
-}
-
-func ShowMovie(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	movie_id := params["id"]
-	fmt.Fprintf(w, "Movie %s loaded", movie_id)
 }
